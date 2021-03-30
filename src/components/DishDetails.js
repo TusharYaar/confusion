4f62559e020a6fb6ffc9eb489be25c6eb8c1d318
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2021-03-30 12:35:30 +0530

    react animations

--- src/components/DishDetails.js
+++ src/components/DishDetails.js
@@ -20,39 +20,43 @@ import {Link} from "react-router-dom";
 import { Loading } from './LoadingComponent';
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function RenderDish({ dish }) {
   return (
-    <div className="col-12 col-md-5 m-1">
-      <Card>
-      <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>
-            {" "}
-            <h4>{dish.name}</h4>
-          </CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
-    </div>
+<div className="col-12 col-md-6">    <FadeTransform
+    in
+    transformProps={{
+        exitTransform: 'scale(0.5) translateY(-50%)'
+    }}>
+<Card>
+    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+    <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
+        <CardText>{dish.description}</CardText>
+    </CardBody>
+</Card>
+</FadeTransform>
+</div>
   );
 }
 
 function RenderComments({ comments }) {
-  return comments.map((comment) => {
-    return (
-      <div key={`c-${comment.id}`} className="my-3">
-        {" "}
-        <div>{comment.comment}</div>
-        --{comment.author},{" "}
-        {new Intl.DateTimeFormat("en-US", {
-          year: "numeric",
-          month: "short",
-          day: "2-digit",
-        }).format(new Date(Date.parse(comment.date)))}
-      </div>
-    );
-  });
+  return (
+    
+    <Stagger in>
+    {comments.map((comment) => {
+        return (
+            <Fade in>
+            <li key={comment.id}>
+            <p>{comment.comment}</p>
+            <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+            </li>
+            </Fade>
+        );
+    })}
+    </Stagger>
+  )
 }
 
 function RenderAll({ props, onclick }) {
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -9,6 +9,7 @@ import {
   CardSubtitle
 } from "reactstrap";
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 function RenderCard({item, isLoading, errMess}) {
     
   if (isLoading) {
@@ -23,14 +24,21 @@ function RenderCard({item, isLoading, errMess}) {
   }
   else 
       return(
-          <Card>
-               <CardImg src={baseUrl + item.image} alt={item.name} />
-              <CardBody>
-              <CardTitle>{item.name}</CardTitle>
-              {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
-              <CardText>{item.description}</CardText>
-              </CardBody>
-          </Card>
+
+        <FadeTransform
+        in
+        transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
+        <Card>
+            <CardImg src={baseUrl + item.image} alt={item.name} />
+            <CardBody>
+            <CardTitle>{item.name}</CardTitle>
+            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+            <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    </FadeTransform>
       );
 
 }
@@ -40,11 +48,17 @@ function Home(props) {
   return(
       <div className="container">
         <h4>Home</h4>
+        <div className="row">
         <div className="col-12 col-md-4">
         <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />
+        </div>
+        <div className="col-12 col-md-4">
         <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} />
 </div>
-      </div>
+<div className="col-12 col-md-4">
+        <RenderCard item={props.leader} isLoading={props.promoLoading} errMess={props.promoErrMess} />
+</div>
+</div>      </div>
     );
 }
 
